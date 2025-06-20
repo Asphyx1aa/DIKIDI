944c697c4e1ab251336b0892efe75237e108fbe4
FargusDN <dmitry.nikulin@mail.ru>
2025-06-20 14:35:34 +0300

    Кастыль для работы не подгружалось /favicon.ico

--- src/test/java/extensions/LoginExtension.java
+++ src/test/java/extensions/LoginExtension.java
@@ -24,7 +24,7 @@ public class LoginExtension implements BeforeEachCallback {
 
         setAuthResponse(authResponse);
 
-        open("/favicon.ico");
+        open("https://dikidi.tech/ru/");
 
         // Преобразуем в Selenium Cookie
         Cookie seleniumCookie = new Cookie.Builder("token", cookieToken)
