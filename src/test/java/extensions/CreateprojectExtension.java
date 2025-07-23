08e98536851facc049e129f679b7c248a606e315
fargus <dmitry.nikylin@mail.ru>
2025-07-23 10:34:09 +0300

    Добавление анатации на создание проекта

---
+++ src/test/java/extensions/CreateprojectExtension.java
@@ -0,0 +1,40 @@
+package extensions;
+
+import data.UserData;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.openqa.selenium.Cookie;
+
+import static api.AuthSteps.getUserAuth;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static context.AuthContext.setAuthResponse;
+
+public class CreateprojectExtension implements BeforeEachCallback {
+
+//    @Override
+    public void beforeEach(ExtensionContext context) {
+
+//        final String userNumber = UserData.fromConfig().getUserNumber();
+//        final String userPassword = UserData.fromConfig().getUserPassword();
+//
+//        Response authResponse = getUserAuth(userNumber, userPassword);
+//        String cookieToken = authResponse.getCookie("token");
+//
+//        setAuthResponse(authResponse);
+//
+//        open("https://dikidi.tech/favicon.svg");
+//
+//        // Преобразуем в Selenium Cookie
+//        Cookie seleniumCookie = new Cookie.Builder("token", cookieToken)
+//                .domain("dikidi.tech")
+//                .path("/")
+//                .isHttpOnly(true)
+//                .isSecure(true)
+//                .build();
+//
+//        getWebDriver().manage().addCookie(seleniumCookie);
+    }
+
+}
